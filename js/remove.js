130e253f27ee37d0732429b33f8a7626c4272d83
kava <codekava@gmail.com>
2023-09-25 23:20:33 +0300

    first commit

---
+++ js/remove.js
@@ -0,0 +1,13 @@
+function removeElement(array, item) {
+	const index = array.indexOf(item);
+	if (index !== -1) {
+		 array.splice(index, 1);
+	}
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 3);
+console.log(array); 
+
+
+// Результат: [ 1, 2, 4, 5, 6, 7 ]
